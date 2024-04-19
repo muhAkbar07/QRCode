e0ad300be2fb030bc087857b6b868974c982e46b
muhAkbar07 <muhakbar072x@gmail.com>
2024-04-19 16:35:18 +0700

    mengubah background

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function generateQR() {
   var nomorTransaksi = document.getElementsByName("nomor_transaksi")[0].value;
-  var typeBarang = document.getElementsByName("type_barang")[0].value;
+  var deskripsiBarang = document.getElementsByName("deskripsi_barang")[0].value;
   var imei = document.getElementsByName("imei")[0].value;
   var namaPengguna = document.getElementsByName("nama_pengguna")[0].value;
 
-  var dataString = `${nomorTransaksi}, ${typeBarang}, ${imei}, ${namaPengguna}`;
+  var dataString = `No. Transaksi = ${nomorTransaksi}\nDeskripsi Barang = ${deskripsiBarang}\nImei = ${imei}\nNama Pengguna = ${namaPengguna}`;
   var url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${dataString}`;
 
   var qrImg = document.getElementById("img");
@@ -21,17 +21,25 @@ function generateQR() {
 function printQR() {
   var qrContainer = document.getElementById("qrContainer");
   var qrImgSrc = qrContainer.querySelector("#img").src;
+  var faviconImgSrc = qrContainer.querySelector('.icon').src;
+
 
   // Buka jendela baru untuk mencetak
   var printWindow = window.open("", "_blank");
-  printWindow.document.write("<html><head><title>Print QR Code</title></head><body>");
+  printWindow.document.write("<html><head><title>QR Code Raja Gadai</title></head><body>");
 
   // Tambahkan gambar QR ke dokumen baru
   printWindow.document.write('<img src="' + qrImgSrc + '" style="display: block; margin: auto;"/>');
 
-  printWindow.document.write("</body></html>");
-  printWindow.document.close();
+  // Tunggu beberapa saat sebelum menambahkan gambar favicon
+  setTimeout(function () {
+    // Tambahkan gambar favicon ke dokumen baru
+    printWindow.document.write('<img src="' + faviconImgSrc + '" style="display: none;"/>');
 
-  // Cetak dokumen
-  printWindow.print();
+    printWindow.document.write("</body></html>");
+    printWindow.document.close();
+
+    // Cetak dokumen
+    printWindow.print();
+  }, 500); // tunggu 500 milidetik (Anda bisa menyesuaikan waktu tunggu sesuai kebutuhan)
 }
